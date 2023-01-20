ef44d6843f710ba2a7002e8c846f5ffc37eaf550
austinmei5 <austinmei@uchicago.edu>
2023-01-20 07:24:10 +0000

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :own_tasks, class_name: "Task", foreign_key: "owner_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
 
-  # resources :tasks
+  resources :tasks
 end
