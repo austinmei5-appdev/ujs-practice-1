037e84ac68c52564c7de7098fe52a86e57473d0e
austinmei5 <austinmei@uchicago.edu>
2023-01-20 23:38:12 +0000

    starting bootstrap

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  status     :string           default("Not yet started")
+#  text       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  owner_id   :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_owner_id  (owner_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (owner_id => users.id)
+#
 class Task < ApplicationRecord
   belongs_to :owner, class_name: "User"
 end
