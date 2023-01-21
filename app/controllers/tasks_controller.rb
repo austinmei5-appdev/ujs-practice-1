07cbf15ac5ad46d758db0f53d20bba42e8ac72b4
austinmei5 <austinmei@uchicago.edu>
2023-01-21 04:53:05 +0000

    create is kinda working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,6 +32,7 @@ class TasksController < ApplicationController
       if @task.save
         format.html { redirect_to root_url, notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
